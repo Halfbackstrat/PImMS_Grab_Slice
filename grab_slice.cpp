4b529c94295f42d0348082a41c7569f50f2c80b6
Halfbackstrat <mattbain17@gmail.com>
2016-03-17 20:50:03 +0000

    First commit

---
+++ grab_slice.cpp
@@ -0,0 +1,61 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+//Declarations and definitions of variables
+int x, y, t, shot, bin;
+vector<vector<int> > image(72, vector<int> (72,0)); 
+
+int main(int argc, char *argv[])
+
+{
+
+ifstream myfile;
+myfile.open(argv[1]);
+
+if (myfile.is_open())
+{
+	while(myfile >> x >> y >> t >> shot >> bin)
+	{	
+		if (t == *argv[2])
+		{
+			if ( x < 72 && y < 72 && x > 0 && y > 0)
+			{
+			++image[x-1][y-1];
+			}
+			else 
+			{
+				cout << "x = " << x << " y = " << y << endl;
+			}
+		}
+		 
+	}
+
+}
+else
+{
+	return 0;
+}
+
+myfile.close();
+
+ofstream outputfile("output");
+
+for (int i = 0; i < image.size(); ++i)
+{
+	for (int j = 0; j < image[i].size(); ++j)
+	{
+		outputfile << image[i][j] << "\t";
+	}
+
+	outputfile << endl;
+}
+
+outputfile.close();
+
+return 0;
+
+}
+
