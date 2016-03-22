1c8c4814c074798a4b87ea40cd3449ec670bc4ca
Halfbackstrat <mattbain17@gmail.com>
2016-03-22 13:57:24 +0000

    After Andy comments

--- grab_slice.cpp
+++ grab_slice.cpp
@@ -7,30 +7,44 @@ using namespace std;
 //Declarations and definitions of variables
 int x, y, t, shot, bin;
 vector<vector<int> > image(72, vector<int> (72,0)); 
-
+int tcount = 0;
+int var;
 int main(int argc, char *argv[])
 
 {
 
+if (argc < 3)
+{return 0;}
+
 ifstream myfile;
 myfile.open(argv[1]);
 
+int var;
+
+var = stoi(argv[2]);
+
+cout << "var = " << var << endl;
 if (myfile.is_open())
 {
-	while(myfile >> x >> y >> t >> shot >> bin)
-	{	
-		if (t == *argv[2])
+	while(myfile >> x >> y >> t >> shot)
+	{
+		//cout << "x = " << x << " y = " << y << " t = " << t << " shot = " << shot << " bin = " << bin << endl;
+		//cout << "t = " << t << " arg2 = " << argv[2] << endl;
+		if (t == var)
 		{
+			tcount++;
+			//cout << tcount << endl;
 			if ( x < 72 && y < 72 && x > 0 && y > 0)
 			{
-			++image[x-1][y-1];
+			//++tcount;
+			image[x-1][y-1]++;
 			}
 			else 
 			{
 				cout << "x = " << x << " y = " << y << endl;
 			}
 		}
-		 
+			 
 	}
 
 }
@@ -38,6 +52,8 @@ else
 {
 	return 0;
 }
+
+cout << "tcount = " << tcount << endl;
 
 myfile.close();
 
